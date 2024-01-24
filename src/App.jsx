c80375f0fabd6eba6e49aafb1fd2ea392224c9f0
hcrymng <haceryiming@gmail.com>
2024-01-24 18:26:11 +0300

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      
-    <h1>Fashion Shoe Shop</h1>
-    </>
-  )
-}
+const App = () => (
+  <main></main>
+)
 
 export default App
