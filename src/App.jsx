1c44db233cb223d727fc02e8f937a3ad0b64c937
hcrymng <haceryiming@gmail.com>
2024-01-24 20:55:44 +0300

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-
+import { Footer, Nav, Hero, Services, Subscribe, SuperQuality, PopularProducts, CustomerReviews, SpecialOffer  } from './sections'
 
 
 const App = () => (
-  <main></main>
+  <main>
+    <section></section>
+    
+  </main>
 )
 
 export default App
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+export {
+    Hero,
+    Nav,
+    Footer,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews
+}
