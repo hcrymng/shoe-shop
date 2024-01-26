faaad273aa0a3b8354764895e8d014e6631e186e
hcrymng <haceryiming@gmail.com>
2024-01-26 13:09:50 +0300

    updating

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const  navLinks =[
+    {href:"#home", lable: "Home"},
+{href:"#about-us", lable: "About Us"},
+{href:"#products", lable: "Products"}
+,{href:"#contact-us", lable: "Contact Us"}
+
+
+]
