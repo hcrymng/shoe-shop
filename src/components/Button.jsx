1c53622019e57c1e7c05ae3dc9745c160b320e76
hcrymng <haceryiming@gmail.com>
2024-01-29 15:41:37 +0300

    updating

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-
+{/* eğer icon varsa button da icon gösterilecek eğer icon verilmediyse iconsız render edilecek */}
 const Button = ({label, iconURL}) => {
   return (
     <button
     className="flex justify-center items-center bg-coral-red text-white gap-2 px-5 py-3
     border font-montserrat text-lg leading-none rounded-full border-coral-red">
 {label}
+{
+  iconURL && 
 <img 
+
+
 src={iconURL}
-/>
+/>}
     </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import Button from '../components/Button'
+import { shoe8 } from '../assets/images'
 const SuperQuality = () => {
   return (
    <section id='about-us' className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
 <div className='flex flex-1 flex-col'>
-  <h2><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla sizi buluşturuyoruz! Hemen gel ve tarzını keşfet!</h2>
-  <p></p>
-
+  <h2 className="mt-10 font-palanquin 
+text-4xl max-sm:text-[72px] max-sm:leading-[70] font-bold"><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla sizi buluşturuyoruz!<br/>Hemen gel ve tarzını keşfet!</h2>
+  <p className='mt-4 lg:max-w-lg info-text'>Premium konfor ve stil sağlamak için titizlikle tasarlanmış ayakkabılarımız, deneyiminizi yükseltmek üzere tasarlanmıştır ve size eşsiz kalite, yenilik ve biraz da zarafet sunar. Her adımda rahatlığı ve şıklığı hissetmek için tasarlanmış olan ayakkabılarımız, her detayıyla özenle üretilmiştir.</p>
+  <p className='info-text mt-6 lg:max-w-lg'>Kalite ve şıklığı bir arada sunan ayakkabılarımız, tarzınızı tamamlayacak ve her adımda kendinizi özel hissetmenizi sağlayacak. Bu ayakkabılarla her adımınızda kendinizi özel ve özgün hissedeceksiniz.</p>
+<div className='mt-11'><Button label="Deytay İçin Tıklayınız"/></div>
 </div>
-
+<div className='flex-1 flex justify-center items-center'> <img  src={shoe8} width={570} height={522} className='object-contain'/></div>
 
    </section>
   )
