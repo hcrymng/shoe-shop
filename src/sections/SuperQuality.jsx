bb525fd0982c29512003d069507fa7ea09b9054a
hcrymng <haceryiming@gmail.com>
2024-01-29 15:14:46 +0300

    updating

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -4,7 +4,7 @@ const SuperQuality = () => {
   return (
    <section id='about-us' className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
 <div className='flex flex-1 flex-col'>
-  <h2><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla seni buluşturuyoruz! Hemen gel ve tarzını keşfet!</h2>
+  <h2><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla sizi buluşturuyoruz! Hemen gel ve tarzını keşfet!</h2>
   <p></p>
 
 </div>
