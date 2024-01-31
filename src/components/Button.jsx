5549d366267ac595d3dee12490dac71203866c97
hcrymng <haceryiming@gmail.com>
2024-01-31 13:44:34 +0300

    updating

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 {/* eğer icon varsa button da icon gösterilecek eğer icon verilmediyse iconsız render edilecek */}
-const Button = ({label, iconURL, bgColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, bgColor, textColor, borderColor, fullWidth}) => {
   return (
     <button
     className={`flex justify-center items-center gap-2 px-5 py-3
-    border font-montserrat text-lg leading-none rounded-full ${bgColor ? `${bgColor}`:  'bg-coral-red'}  ${borderColor ? `${borderColor}`: 'border-coral-red' } ${textColor ? `${textColor}` :   'text-white'}`}>
+    border font-montserrat text-lg leading-none rounded-full ${bgColor ? `${bgColor}`:  'bg-coral-red'}  ${borderColor ? `${borderColor}`: 'border-coral-red' } ${textColor ? `${textColor}` :   'text-white'} ${fullWidth && 'w-full'}`}>
 {label}
 {
   iconURL && 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,12 +1,12 @@
 import Button from "../components/Button"
 const Subscribe = () => {
   return (
-    <section className="flex max-container justify-between items-center">
-      <h3 className="flex-1 "><span className="text-coral-red">Yeni Ürünlerimizden</span> Haberdar Olmak için Üye Olmayı Unutmayın!</h3>
-<form className="flex-1 flex justify-between items-center " >
-<input placeholder="email.com" className="        input"/>
-<Button label="Oturum Aç" />
-</form>
+    <section className="flex max-container justify-between items-center max-lg:flex-col gap-10">
+      <h3 className="text-4xl leading-[68px] font-palanquin font-bold lg:max-w-md "><span className="text-coral-red">İndirim</span> ve <span className="text-coral-red">Kampanyalardan</span> Haberdar Olmak için Üye Olmayı Unutmayın!</h3>
+<div  className="flex-1 flex justify-between items-center lg:max-w-[40%] w-full max-sm:flex-col gap-5 p-2.5  sm:border border-slate-gray rounded-full" >
+<input placeholder="subscribe@modaayakkabi.com" className="input"   type="text"/>
+<div className="flex max-sm:justify-end items-center max-sm:w-full"><Button label="Oturum Aç" fullWidth /></div>
+</div>
 
     </section>
   )
