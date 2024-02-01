ca34be0185331ad5755d4a003c56df017153896d
hcrymng <haceryiming@gmail.com>
2024-02-01 22:30:10 +0300

    updating

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -6,7 +6,7 @@ const CustomerReviews = () => {
    <section className='max-container'>
     <div className='flex flex-col justify-center items-center'>
     <h2 className="mt-10 font-palanquin 
-text-4xl max-sm:text-[72px] max-sm:leading-[70] font-bold "><span className='text-coral-red'>Müşteri</span> Yorumları</h2>
+text-4xl max-sm:text-[72px] max-sm:leading-[70px] font-bold "><span className='text-coral-red'>Müşteri</span> Yorumları</h2>
   <p className='mt-4 lg:max-w-lg text-center info-text'>Değerli müşterilerimizin olağanüstü deneyimleri hakkında gerçek hikayeleri dinleyin. Sizi de memnun edecek bir deneyim yaşamak için hemen bizimle iletişime geçin.</p>
     </div>
     <div className='flex mt-10 max-lg:flex-col'>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -6,7 +6,7 @@ const SuperQuality = () => {
    <section id='about-us' className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
 <div className='flex flex-1 flex-col'>
   <h2 className="mt-10 font-palanquin 
-text-4xl max-sm:text-[72px] max-sm:leading-[70] font-bold"><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla sizi buluşturuyoruz!<br/>Hemen gel ve tarzını keşfet!</h2>
+text-4xl max-sm:text-[72px] max-sm:leading-[70px] font-bold"><span className='text-coral-red'>Süper kaliteli</span> <br/>ayakkabılarımızla sizi buluşturuyoruz!<br/>Hemen gel ve tarzını keşfet!</h2>
   <p className='mt-4 lg:max-w-lg info-text'>Premium konfor ve stil sağlamak için titizlikle tasarlanmış ayakkabılarımız, deneyiminizi yükseltmek üzere tasarlanmıştır ve size eşsiz kalite, yenilik ve biraz da zarafet sunar. Her adımda rahatlığı ve şıklığı hissetmek için tasarlanmış olan ayakkabılarımız, her detayıyla özenle üretilmiştir.</p>
   <p className='info-text mt-6 lg:max-w-lg'>Kalite ve şıklığı bir arada sunan ayakkabılarımız, tarzınızı tamamlayacak ve her adımda kendinizi özel hissetmenizi sağlayacak. Bu ayakkabılarla her adımınızda kendinizi özel ve özgün hissedeceksiniz.</p>
 <div className='mt-11'><Button label="Deytay İçin Tıklayınız"/></div>
